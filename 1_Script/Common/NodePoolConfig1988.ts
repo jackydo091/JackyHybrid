5072ada06c98889ebe184e44da086df9f14ff9f1
jackydo091 <an.do@gemi.io>
2022-02-16 17:20:53 +0700

    Update bullet ingame

--- 1_Script/Common/NodePoolConfig1988.ts
+++ 1_Script/Common/NodePoolConfig1988.ts
@@ -23,7 +23,7 @@ export default class NodePoolConfig1988 extends gfNodePoolConfig {
             "8" :  {speed: 60,    FishMultiple: -1,     zIndex: 302, visibleSize:   v2(120, 80),  targetPoint: v2(10, 0),      BoxCollider: [new Rect(-8.5, 0, 111, 15), new Rect(6.6, 0, 66, 40)]},
             "9" :  {speed: 60,    FishMultiple: -1,     zIndex: 308, visibleSize:   v2(100, 70),  targetPoint: v2(-10, 0),       BoxCollider: new Rect(-15, 3, 70, 51)},
             "10" : {speed: 60,    FishMultiple: -1,     zIndex: 304, visibleSize:   v2(85, 90),  targetPoint: v2(10, 0),       BoxCollider: new Rect(12, 0, 40, 45)},
-            "11" : {speed: 60,    FishMultiple: -1,     zIndex: 209, AnimationName: ['animation'], visibleSize:   v2(200, 70),  targetPoint: v2(10, 0),       BoxCollider: new Rect(-39.6, 0, 132, 35)},
+            "11" : {speed: 60,    FishMultiple: -1,     zIndex: 209, visibleSize:   v2(200, 70),  targetPoint: v2(10, 0),       BoxCollider: new Rect(-39.6, 0, 132, 35)},
             "12" : {speed: 60,    FishMultiple: -1,     zIndex: 206, visibleSize:   v2(160, 115),  targetPoint: v2(15, 0),       BoxCollider: new Rect(-2, 0, 117, 29)},
             "13" : {speed: 40,    FishMultiple: -1,     zIndex: 202, visibleSize:   v2(155, 75),  targetPoint: v2(10, 0),      BoxCollider: [new Rect(-12.4, 0, 120, 19), new Rect(6.5, 2, 66, 46)]},
             "14" : {speed: 40,    FishMultiple: -1,     zIndex: 203, visibleSize:   v2(130, 100),  targetPoint: v2(20, 0),      BoxCollider: new Rect(19, 0, 80, 80)},
@@ -44,7 +44,7 @@ export default class NodePoolConfig1988 extends gfNodePoolConfig {
             "46" : {speed: 20,    FishMultiple: -1,     zIndex: 104, AnimationName: ['animation'], visibleSize: v2(350, 250),   targetPoint: v2(17, 0),       BoxCollider: [new Rect(0, 0, 230, 50),new Rect(-16.3, -41, 110.6, 32), new Rect(0, 41.4, 60, 33)]},
         };
 
-        this.SPRITE_FISH_KIND = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+        this.SPRITE_FISH_KIND = [0,1,2,3,4,5,6,7,8,9,10,15,12,13,14];
         this.BULLET_CONFIG = {
             "0" : {BoxCollider: new Rect(-25, 0, 50, 15)},
             "1" : {BoxCollider: new Rect(-30, 0, 60, 25)},
